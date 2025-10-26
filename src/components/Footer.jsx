b363cf538ee3c357154c7f4bc14122098b46796e
Umar-Farooq-5757 <aamirgel17@gmail.com>
2025-10-26 20:54:31 +0500

    UI fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-const Footer = ({resData}) => {
+const Footer = ({ resData }) => {
   return (
-    <footer className={`w-full bg-white absolute ${Object.keys(resData).length?'-bottom-[70px]':'-bottom-[177px]'} right-0 left-0 text-center py-2 border border-gray-300`}>
+    <footer
+      className={`w-full bg-white absolute ${
+        Object.keys(resData).length ? "-bottom-[70px]" : "-bottom-[137px] sm:-bottom-[186px] md:-bottom-[177px]"
+      } right-0 left-0 text-center py-2 border border-gray-300`}
+    >
       Made with ❤️ by Umar Farooq
     </footer>
   );
